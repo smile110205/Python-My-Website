4ea71fcc6a89d6079ac125c36575aaa870b0fb60
smile110205 <99063740+smile110205@users.noreply.github.com>
2024-04-23 12:50:33 +0000

    Added Database

---
+++ database.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine, text
+
+engine = create_engine(
+    "mariadb+pymysql://root:!nspir3P3ns@10.10.9.121/wbt?charset=utf8mb4")
+
+# with engine.connect() as conn:
+#     result = conn.execute(text("select * from jobs"))
+#     result_all = result.all()
+#     first_result = result_all[0]
+#     column_names = result.keys()
+#     first_result_dict = dict(zip(column_names, first_result))
+#     print(first_result_dict)
+
+# with engine.connect() as conn:
+#   result = conn.execute(text("select * from jobs"))
+#   column_names = result.keys()
+
+#   result_dicts = []
+
+#   for row in result.all():
+#     result_dicts.append(dict(zip(column_names, row)))
+#   print(result_dicts)
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    column_names = result.keys()
+
+    jobs = []
+
+    for row in result.all():
+      jobs.append(dict(zip(column_names, row)))
+    return jobs
